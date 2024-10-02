193a4da8db91fb7e1d3d370ff92dec71658fcb0f
Ryuji Takamura <110015984+Ryuji-arch@users.noreply.github.com>
2024-10-02 19:12:32 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,6 @@ var vg_1 = "annual-energy-consumption-by-countries.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {
     }).catch(console.error);
 
-var vg_2 = "access-to-electricity-by-country.vg.json";
+var vg_2 = "access-to-electricity-by-countries.vg.json";
 vegaEmbed("#map2", vg_2).then(function(result) {
     }).catch(console.error);
