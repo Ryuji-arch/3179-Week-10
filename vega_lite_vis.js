3adc11fc63a0f8638ccff955166dad01079aeedf
Ryuji Takamura <110015984+Ryuji-arch@users.noreply.github.com>
2024-10-02 18:27:09 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,7 @@
 var vg_1 = "Energy_Consumption_By_Countries.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {
     }).catch(console.error);
+
+var vg_2 = "access-to-electricity-by-country.vg.json";
+vegaEmbed("#map2", vg_2).then(function(result) {
+    }).catch(console.error);
